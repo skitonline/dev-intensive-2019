140a7ce0defdb7296c47fd0677ac668648cb65a3
Alexander Evseev <skitonline@yandex.ru>
2019-05-13 10:14:47 +0400

    done 4

---
+++ src/main/java/net/thumbtack/school/figures/v1/ColoredRectangle.java
@@ -0,0 +1,54 @@
+package net.thumbtack.school.figures.v1;
+
+import java.util.Objects;
+
+public class ColoredRectangle extends Rectangle{
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    private int color;
+
+    public ColoredRectangle(Point leftTop, Point rightBottom, int color){
+        super(leftTop, rightBottom);
+        this.color = color;
+    }
+
+    public ColoredRectangle(int xLeft, int yTop, int xRight, int yBottom, int color){
+        super(xLeft, yTop, xRight, yBottom);
+        this.color = color;
+    }
+
+    public ColoredRectangle(int length, int width, int color){
+        super(length, width);
+        this.color = color;
+    }
+
+    public ColoredRectangle(int color){
+        super();
+        this.color = color;
+    }
+
+    public ColoredRectangle(){
+        super();
+        color = 1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ColoredRectangle that = (ColoredRectangle) o;
+        return color == that.color;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), color);
+    }
+}
